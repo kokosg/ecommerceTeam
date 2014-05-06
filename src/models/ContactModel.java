a266f511d6d644d806921e7f4849da22bb70d5a0
Shreebha <shreebha.arora@gmail.com>
2014-05-06 14:22:18 +0100

    Merge branch 'master' of https://github.com/kokosg/ecommerceTeam

--- src/models/ContactModel.java
+++ src/models/ContactModel.java
@@ -6,6 +6,7 @@ package models;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Properties;
 
 import javax.mail.Message;
@@ -15,6 +16,8 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+
+import objects.MessageObject;
 
 /**
  * @author Master Team 10
@@ -30,24 +33,86 @@ public class ContactModel {
 	}
 	
 	
-	public void insertEmail(String name, String title, String email, String message) throws SQLException {
-		  
-		String insertQuery = "INSERT INTO Message (name, title, email, message) VALUE ('" + name + "', '" + title + "', '" + email + "', '" + message + "')";
-			
+	public ArrayList<MessageObject> getUnpublishMessages() throws SQLException {
+		ArrayList<MessageObject> arrayResults = new ArrayList<MessageObject>();
+		String queryAuthor = "select messageID, name, title, email, message, answer, published from Message where published = 0";
 		try {
-			
+			ConnectionManager conn = new ConnectionManager();
+			Statement st = conn.getInstance().getConnection().createStatement();
+			ResultSet rs = st.executeQuery(queryAuthor);
+			while (rs.next()) {
+				int messageID = rs.getInt("messageID");
+				String name = (String) rs.getObject("name");
+				String title = (String) rs.getObject("title");
+				String email = (String) rs.getObject("email");
+				String message = (String) rs.getObject("message");
+				String answer = (String) rs.getObject("answer");
+				boolean published = (boolean) rs.getObject("published");
+
+				
+				MessageObject messageObject = new MessageObject(messageID, name, title, email, message, answer, published);
+				arrayResults.add(messageObject);
+			}
+			rs.close();
+			st.close();
+			conn.close();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return arrayResults;
+	}
+	
+	public void insertEmail(String name, String title, String email, String message) throws SQLException {
+		String insertQuery = "INSERT INTO Message (name, title, email, message) VALUE ('" + name + "', '" + title + "', '" + email + "', '" + message + "')";
+		try {
 	      ConnectionManager conn = new ConnectionManager();
     	  Statement st = conn.getInstance().getConnection().createStatement();
 		  st.executeUpdate(insertQuery);
-		  
 	      st.close();
 		  conn.close();
-			
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	
+	public boolean updateMessage(String messageID) throws SQLException {
 		
+		boolean status = false;
+		
+		try {
+			ConnectionManager conn = new ConnectionManager();
+			Statement st = conn.getInstance().getConnection().createStatement();
+			String updateQuery = "UPDATE Message SET published = 1 WHERE messageID = '" + messageID + "'";
+			st.executeUpdate(updateQuery);
+			st.close();
+			conn.close();
+			
+			status = true;
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		return status;
+	}
+	
+	
+	public boolean deleteMessage(String messageID) throws SQLException {
+		boolean status = false;
+		try {
+			ConnectionManager conn = new ConnectionManager();
+			Statement st = conn.getInstance().getConnection().createStatement();
+			String deleteQuery = "DELETE FROM Message WHERE messageID = '" + messageID + "'";
+			st.executeUpdate(deleteQuery);
+			st.close();
+			conn.close();
+			status = true;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return status;
 	}
 	
 	
@@ -87,7 +152,7 @@ public class ContactModel {
 					MimeMessage message = new MimeMessage(session);
 					message.setFrom(new InternetAddress(editorEmail));// change accordingly
 					message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-					message.setSubject("Automatic Message - Java ecommarce");
+					message.setSubject("Automatic Message - Java ecommerce");
 					message.setText("Name: " + name + "\n" + "email:" + email + "\n" + "Title: " + title + "\n" + "Message: "+"\n" + messageText);
 
 					// send message
@@ -110,5 +175,43 @@ public class ContactModel {
 		return emailStatus;
 		
 	}
+	
+	public boolean sendRegistrationEmail(String email, String messageText) throws ClassNotFoundException, SQLException {
+		
+		boolean emailStatus = false;
+		
+		String to = email;// change accordingly
 
+		// Get the session object
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.port", "465");
+
+		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("javaecom@gmail.com","teammaster10");// change accordingly
+			}
+		});
+
+		// compose message
+		try {
+			MimeMessage message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("javaecom@gmail.com"));// change accordingly
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+			message.setSubject("Automatic Message - Journal Registration");
+			message.setText("Message: "+"\n" + messageText);
+			// send message
+			Transport.send(message);
+				
+			emailStatus = true;
+		} catch (MessagingException e) {
+			emailStatus = false;
+			throw new RuntimeException(e);
+		}
+		System.out.println(emailStatus);
+		return emailStatus;
+	}
 }
