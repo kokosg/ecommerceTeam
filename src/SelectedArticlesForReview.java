3ea2eea618645fb1b9e6f11d17fe66c5b49dfeb8
Renuka Pahade <renuka@dyn196153.shef.ac.uk>
2014-05-08 12:33:10 +0100

    response by reviewer

--- src/SelectedArticlesForReview.java
+++ src/SelectedArticlesForReview.java
@@ -1,6 +1,6 @@
 /**
-* Servlet implementation class SelectedArticlesForReview
-*/
+ * Servlet implementation class SelectedArticlesForReview
+ */
 
 import java.util.ArrayList;
 
@@ -17,41 +17,41 @@ import org.apache.velocity.context.Context;
 import org.apache.velocity.tools.view.VelocityViewServlet;
 
 public class SelectedArticlesForReview extends VelocityViewServlet {
-private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-public Template handleRequest( HttpServletRequest request, HttpServletResponse response, Context context ) {
-Template template=null;
-HttpSession session = request.getSession();
-int authorID =(Integer) session.getAttribute("userID");
-AbstractModel absModel=new AbstractModel();
-ReviewForm form = new ReviewForm();
+	public Template handleRequest( HttpServletRequest request, HttpServletResponse response, Context context ) {
+		Template template=null;
+		HttpSession session = request.getSession();
+		int authorID =(Integer) session.getAttribute("userID");
+		AbstractModel absModel=new AbstractModel();
+		ReviewForm form = new ReviewForm();
 
-ArrayList<Integer> downloadedReview = absModel.getDownloaded(authorID);
-//context.put("downloadedReview", downloadedReview);
-//System.out.println("Downloaded ^^^^^^^^ "+downloadedReview);
+		ArrayList<Integer> downloadedReview = absModel.getDownloaded(authorID);
+		//context.put("downloadedReview", downloadedReview);
+		//System.out.println("Downloaded ^^^^^^^^ "+downloadedReview);
 
-String unselect = request.getParameter("unselect");
-if(unselect!=null){
-System.out.println("String: selected servlet="+unselect);
-absModel.deleteChoice(authorID, Integer.parseInt(unselect));
-}
-ArrayList<Article> checkTitle =absModel.getChoiceTitle(authorID);
-for(Article result :checkTitle ){
-for(int reviewDwnld : downloadedReview){
-if(reviewDwnld==result.getArticleID()){
-result.setDownloaded(true);
-result.setReviewCount(form.getReviewCount(authorID, reviewDwnld));
-}
-}
-System.out.println("Title :"+ result.getTitle() +" Summary: "+result.getSummary()+ " Chosen  :" + result.isChosen() );
-}
+		String unselect = request.getParameter("unselect");
+		if(unselect!=null){
+			System.out.println("String: selected servlet="+unselect);
+			absModel.deleteChoice(authorID, Integer.parseInt(unselect));
+		}
+		ArrayList<Article> checkTitle =absModel.getChoiceTitle(authorID);
+		for(Article result :checkTitle ){
+			for(int reviewDwnld : downloadedReview){
+				if(reviewDwnld==result.getArticleID()){
+					result.setDownloaded(true);
+					result.setReviewCount(form.getReviewCount(authorID, reviewDwnld));
+				}
+			}
+			System.out.println("Title :"+ result.getTitle() +" Summary: "+result.getSummary()+ " Chosen  :" + result.isChosen()+"Revision:"+result.isResponsAvailable() );
+		}
 
-if(checkTitle.isEmpty()){
-context.put("message", "You haven't selected any articles to Review. Go to unpublished articles to select articles to review.");
-}
+		if(checkTitle.isEmpty()){
+			context.put("message", "You haven't selected any articles to Review. Go to unpublished articles to select articles to review.");
+		}
 
-context.put("artCkeckId", checkTitle);
-template = getTemplate("/forms/selectedArticlesToReview.vm");
-return template;
+		context.put("artCkeckId", checkTitle);
+		template = getTemplate("/forms/selectedArticlesToReview.vm");
+		return template;
+	}
 }
-  }
