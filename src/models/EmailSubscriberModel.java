0931a8b3c30c37840eaae6052021a994bafffcce
kokosg <sirdevious@gmail.com>
2014-05-09 14:07:54 +0100

    Update EmailSubscriberModel.java

--- src/models/EmailSubscriberModel.java
+++ src/models/EmailSubscriberModel.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 
 import objects.EmailMessage;
 import objects.EmailSubscriber;
-import objects.MessageObject;
 
 /**
  * @author acp13gg
@@ -24,6 +23,53 @@ public class EmailSubscriberModel {
 	public EmailSubscriberModel() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	public ArrayList<EmailSubscriber> sendEmailSubscriber(String subscriberEmail) throws SQLException, ClassNotFoundException {
+		ArrayList<EmailSubscriber> arrayResults = new ArrayList<EmailSubscriber>();
+
+		String query = "select DISTINCT Article.title from Keyword INNER JOIN SubKeyword ON Keyword.keywordID = SubKeyword.keywordID INNER JOIN Subscriber ON Subscriber.subscriberID = SubKeyword.subscriberID INNER JOIN ArticleKeyword ON ArticleKeyword.keywordID = Keyword.keywordID INNER JOIN Article ON Article.articleID = ArticleKeyword.articleID WHERE Subscriber.email = '" + subscriberEmail + "'";
+		
+		ConnectionManager conn = new ConnectionManager();
+		Statement st = conn.getInstance().getConnection().createStatement();
+		ResultSet rs = st.executeQuery(query);
+		
+		try {
+
+				EmailSubscriber emailSubscriber = new EmailSubscriber();
+
+				while (rs.next()) {
+					String title = (String) rs.getObject("Article.title");
+
+					emailSubscriber = new EmailSubscriber(title);
+					arrayResults.add(emailSubscriber);
+				}
+			
+				if (arrayResults.isEmpty()) {
+					//create ContactModel object
+					ContactModel contactModel = new ContactModel();
+					
+					System.out.print("Subscriber " + " Subscriber Email " + " " + subscriberEmail + " " + emailSubscriber.getTitle().toString() + " telos");
+					
+					String messageText = "The following article(s) match with your keyword(s): " + emailSubscriber.getTitle();
+					
+					//create EmailMessage object by passing values
+			    	EmailMessage emailMessage = new EmailMessage("Subscriber", "Subscriber Email", subscriberEmail, messageText);
+			    	  
+			    	//call the method sendEmail from contactModel object and passing values in order to trigger the email function
+					contactModel.sendEmail(emailMessage.getName(), emailMessage.getEmail(), emailMessage.getTitle(), emailMessage.getMessage());
+				} else {
+					System.out.print("is null");
+				}
+				
+			rs.close();
+			st.close();
+			conn.close();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return arrayResults;
+	}
+	
 
 	public ArrayList<EmailSubscriber> sendEmailSubscriber() throws SQLException {
 		ArrayList<EmailSubscriber> arrayResults = new ArrayList<EmailSubscriber>();
@@ -37,7 +83,7 @@ public class EmailSubscriberModel {
 
 			while (rs.next()) {
 				int subscriberID = rs.getInt("subscriberID");
-				boolean editionSubscriber = (Boolean) rs.getObject("editionSubscriber");
+				boolean editionSubscriber = (boolean) rs.getObject("editionSubscriber");
 				int keywordSubscriber = rs.getInt("keywordSubscriber");
 				String email = (String) rs.getObject("email");
 
